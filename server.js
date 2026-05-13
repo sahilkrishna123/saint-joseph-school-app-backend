949a4d8de1fa57ea42e8714e9a67117e671b0e3c
sahilkrishna123 <sahilkrishna1243@gmail.com>
2026-05-13 20:25:04 +0500

    Update package.json

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const PORT = 3000;
+app.get("/", (req, res) => {
+  res.send("Backend running on Render 🚀");
+});
 
-// Start server
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
